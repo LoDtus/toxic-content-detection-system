4552879f3d7b70399732f3f5eb2b2e7744faa3de
Long Nguyen <nguyentrunglong.150903@gmail.com>
2024-11-03 01:43:46 +0700

    root

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import '../App.css'
+
+function Header() {
+    return (
+        <header className="flex justify-center">
+            <div className="container-header flex items-center pl-4 py-4 mb-2 font-semibold">
+                Project: Harmful Content Detection System
+            </div>
+        </header>
+    )
+}
+export default Header;
---
+++ client/src/components/TextArea.js
@@ -0,0 +1,9 @@
+function TextArea() {
+    return (
+        <div className="textArea basis-[70%]">
+            <textarea className="bg-white border resize-y rounded-md
+            w-full min-h-[80vh] px-4 py-3 focus:outline-[#7a7a7a]" placeHolder="Aa"></textarea>
+        </div>
+    )
+}
+export default TextArea;
