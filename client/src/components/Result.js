948cf0255d8e2f528a816c5f3a0a7a9f62ee28a2
Long Nguyen <nguyentrunglong.150903@gmail.com>
2024-11-08 20:56:53 +0700

    Fixed Project Structure

--- client/src/components/Result.js
+++
@@ -1,9 +0,0 @@
-function Result() {
-    return (
-        <div className="result basis-[30%]">
-            <div className="bg-[#3775e6] text-white font-semibold flex items-center justify-center
-                py-3 hover:cursor-pointer hover:bg-[#558aec] duration-200 rounded-md lg:ml-3">Analyze</div>
-        </div>
-    )
-}
-export default Result;
